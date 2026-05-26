1c28a7a80d6166c7d366414d9b20f291add78076
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 19:16:43 -0300

    chore: update shareCode to NAUGU76T3VC

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=DLCWVBULSCJ&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=NAUGU76T3VC&afp=tapib' });
     res.end();
   }
