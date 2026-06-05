796c4a46a717fef2f6635f8247e3c99dad0cfd21
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 10:59:56 -0300

    chore: shareCode IWK4KCRHAVT -> NSQACEYBC0T (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=IWK4KCRHAVT&afp=td' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=NSQACEYBC0T&afp=td' });
     res.end();
   }
